9356851271bed2ac0646cc150d46675a9a96d944
Joao Brito <britojsj@gmail.com>
2018-04-20 11:21:03 -0300

    infer: add conversions and temp files

--- cmd/inference/inference.go
+++ cmd/inference/inference.go
@@ -1,10 +1,17 @@
 package inference
 
 import (
+	"bufio"
 	"flag"
+	"fmt"
 	"log"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/britojr/exp-run/cmd"
+	"github.com/britojr/exp-run/cmd/convert"
+	"github.com/britojr/utl/ioutl"
 )
 
 var Cmd = &cmd.Command{}
@@ -28,5 +35,34 @@ func init() {
 }
 
 func Infer(mFile, qFile, evFile string) {
-	// convert.Convert(mFile, dst, comvert. convType, dsname, smooth)
+	dainame := strings.TrimSuffix(mFile, filepath.Ext(mFile)) + ".uai"
+	convert.Convert(mFile, dainame, convert.Bif2uai, "", "", 0.0)
+	daiEv, daiQu := dainame+".evid", dainame+".quer"
+	qevFile := dainame + ".temp"
+	defer os.Remove(qevFile)
+	mergeQev(qFile, evFile, qevFile)
+	convert.Convert(qevFile, daiQu, convert.Ev2evid, "", "", 0.0)
+	convert.Convert(evFile, daiEv, convert.Ev2evid, "", "", 0.0)
+
+}
+
+func mergeQev(qFile, evFile, qevFile string) {
+	rq := ioutl.OpenFile(qFile)
+	defer rq.Close()
+	rev := ioutl.OpenFile(evFile)
+	defer rev.Close()
+	w := ioutl.CreateFile(qevFile)
+	defer w.Close()
+	scq := bufio.NewScanner(rq)
+	scev := bufio.NewScanner(rev)
+	for scq.Scan() {
+		scev.Scan()
+		line := strings.Split(scev.Text(), ",")
+		for i, v := range strings.Split(scq.Text(), ",") {
+			if v != "*" {
+				line[i] = v
+			}
+		}
+		fmt.Fprintln(w, strings.Join(line, ","))
+	}
 }
