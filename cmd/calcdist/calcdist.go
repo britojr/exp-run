abce703386d061d0a58584f71f1e35c52dc0b0f6
Joao Brito <joaojr.89@gmail.com>
2018-04-29 21:36:31 -0300

    calcdist: add more distances

--- cmd/calcdist/calcdist.go
+++ cmd/calcdist/calcdist.go
@@ -12,17 +12,30 @@ import (
 	"github.com/britojr/exp-run/cmd"
 	"github.com/britojr/utl/ioutl"
 	"github.com/britojr/utl/stats"
+	"github.com/gonum/floats"
+	"gonum.org/v1/gonum/stat"
 )
 
-// distance functions
-const (
-	Mse = "mse" //mean squared error
-)
-
-func DistFuncs() []string {
-	return []string{Mse}
+func distFuncs() (dfs []string) {
+	for name := range distances {
+		dfs = append(dfs, name)
+	}
+	return
 }
 
+// distance functions
+var distances = map[string]func(a, b []float64) float64{
+	"mse":           stats.MSE,
+	"max-abs":       stats.MaxAbsErr,
+	"hellinger":     stats.HellDist,
+	"cross-entropy": stat.CrossEntropy,
+	"kl":            stat.KullbackLeibler,
+	// "hellinger-gonum": stat.Hellinger,
+	"l1norm": func(a, b []float64) float64 { return floats.Distance(a, b, 1) },
+	"l2norm": func(a, b []float64) float64 { return floats.Distance(a, b, 2) },
+}
+
+// Cmd command struct
 var Cmd = &cmd.Command{}
 
 func init() {
@@ -33,9 +46,9 @@ func init() {
 		inFile1 := cm.Flag.String("i1", "", "inference result 1")
 		inFile2 := cm.Flag.String("i2", "", "inference result 1")
 		outFile := cm.Flag.String("log", "", "output file")
-		distOpt := cm.Flag.String("dif", Mse, "distance function ("+strings.Join(DistFuncs(), "|")+")")
+		distOpt := cm.Flag.String("dif", "", "distance function ("+strings.Join(distFuncs(), "|")+")")
 		cm.Flag.Parse(args)
-		if len(*inFile1) == 0 || len(*inFile2) == 0 {
+		if len(*inFile1) == 0 || len(*inFile2) == 0 || len(*distOpt) == 0 {
 			log.Printf("error: missing arguments!\n")
 			cm.Flag.PrintDefaults()
 			return
@@ -44,33 +57,30 @@ func init() {
 	}
 }
 
+// CalcDist calculates distance between values of two files
 func CalcDist(inFile1, inFile2, outFile, distOpt string) {
-	switch distOpt {
-	case Mse:
-		calcMSE(inFile1, inFile2, outFile)
-	default:
-		log.Printf("error: invalid option: (%v)\n\n", distOpt)
-		Cmd.Flag.PrintDefaults()
+	if dist, ok := distances[distOpt]; ok {
+		f1Vals := readInfFile(inFile1)
+		log.Printf("%v: %v values read\n", inFile1, len(f1Vals))
+		f2Vals := readInfFile(inFile2)
+		log.Printf("%v: %v values read\n", inFile2, len(f2Vals))
+		if len(f1Vals) != len(f2Vals) {
+			log.Printf("error: different size\n")
+			return
+		}
+		dVal := dist(f1Vals, f2Vals)
+		if len(outFile) != 0 {
+			f := ioutl.CreateFile(outFile)
+			fmt.Fprintf(f, "%v\n", dVal)
+			f.Close()
+		} else {
+			fmt.Printf("%v\n", dVal)
+		}
 		return
 	}
-}
-
-func calcMSE(inFile1, inFile2, outFile string) {
-	f1Vals := readInfFile(inFile1)
-	log.Printf("%v: %v values read\n", inFile1, len(f1Vals))
-	f2Vals := readInfFile(inFile2)
-	log.Printf("%v: %v values read\n", inFile2, len(f2Vals))
-	if len(f1Vals) != len(f2Vals) {
-		return
-	}
-	mse := stats.MSE(f1Vals, f2Vals)
-	if len(outFile) != 0 {
-		f := ioutl.CreateFile(outFile)
-		fmt.Fprintf(f, "%v\n", mse)
-		f.Close()
-	} else {
-		fmt.Printf("%v\n", mse)
-	}
+	log.Printf("error: invalid option: (%v)\n\n", distOpt)
+	Cmd.Flag.PrintDefaults()
+	return
 }
 
 func readInfFile(fname string) (vs []float64) {
